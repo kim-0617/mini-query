dffb349126b304b3e3b10f30695af59e812a7436
kim-0617 <kkk5993@naver.com>
2023-11-10 18:35:18 +0900

    modify to obj version
    - + modify readme

--- packages/library/src/main.ts
+++ packages/library/src/main.ts
@@ -1,25 +1,20 @@
 export const name = "sh-kim";
-
-class MiniQuery {
-  private elements: Element[];
-
-  constructor(selector: string, container?: Element) {
-    this.elements = Array.from(
-      (container ?? document).querySelectorAll(selector)
-    );
-  }
-
-  length() {
-    return this.elements.length;
-  }
-
-  click(handler: EventListener) {
-    this.elements.forEach((element) => {
-      element.addEventListener("click", handler);
-    });
-  }
-}
-
 export const $ = (selector: string, container?: Element) => {
-  return new MiniQuery(selector, container);
+  const elements = Array.from(
+    (container ?? document).querySelectorAll(selector)
+  );
+
+  // closure
+
+  return {
+    length() {
+      return elements.length;
+    },
+
+    click(handler: EventListener) {
+      elements.forEach((element) => {
+        element.addEventListener("click", handler);
+      });
+    },
+  };
 };
